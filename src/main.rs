d17f97c6855ce72c627e1564a79b3583e9f059e1
Tyson Nottingham <tyson.nottingham@threatx.com>
2023-01-30 15:17:04 -0800

    Add /data endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::Query,
     routing::{get, post},
     Router,
 };
@@ -6,11 +7,23 @@ use axum::{
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(|| async { "GET!" }))
-        .route("/", post(|| async { "POST!" }));
+        .route("/", get(|| async { "GET!\n" }))
+        .route("/", post(|| async { "POST!\n" }))
+        .route("/data", get(data))
+        .route("/data", post(data));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+#[derive(serde::Deserialize)]
+struct Size {
+    size: u16,
+}
+
+async fn data(size: Query<Size>) -> String {
+    let size = size.0.size as usize;
+    "b".to_owned().repeat(size)
 }
